d663c2e60ca9323e0bd6e21b269eb024d559e58e
ankur <best.ankur@gmail.com>
2015-03-11 22:07:55 +0100

    fixed the background as well

--- background/back.py
+++ background/back.py
@@ -5,14 +5,14 @@ class CBackground():
         self.size = size
         self.width = width
         self.color = color
-        self.myimage = pygame.image.load("./data/bg4.png")
+        self.myimage = pygame.image.load("./data/back.png")
         self.imagerect = self.myimage.get_rect()
-        self.imagerect.x,self.imagerect.y = (360,640)
+        self.imagerect.x,self.imagerect.y = (x,y)
         self.x,self.y = self.imagerect.center
-         
+        self.imagerect.center = (x,y)
         self.x, self.y = (x,y)
     def update(self,screen):
-        #screen.blit(self.myimage, self.imagerect)
+        screen.blit(self.myimage, self.imagerect)
         #use it to produce game-over effects for few seconds
         #self.color = self.colorobj.randomcolor()
-        pygame.draw.circle(screen, self.color, (self.x,self.y), self.size, self.width)
+        #pygame.draw.circle(screen, self.color, (self.x,self.y), self.size, self.width)
